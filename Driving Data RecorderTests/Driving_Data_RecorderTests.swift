cf4d23b4d58708e7d0627423c89ee881d58b8f89
Michael Tucker <mictuc@stanford.edu>
2016-01-19 15:28:37 -0800

    Initial Commit

---
+++ Driving Data RecorderTests/Driving_Data_RecorderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Driving_Data_RecorderTests.swift
+//  Driving Data RecorderTests
+//
+//  Created by Michael Tucker on 1/19/16.
+//  Copyright © 2016 DesignX. All rights reserved.
+//
+
+import XCTest
+@testable import Driving_Data_Recorder
+
+class Driving_Data_RecorderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
